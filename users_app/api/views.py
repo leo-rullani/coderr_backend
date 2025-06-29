9b59d307c63a663c066d28cd013106570e29fc78
Leugzim Rullani <leugzimrullani@outlook.com>
2025-06-29 14:56:34 +0200

    Fix profile detail endpoint: Use user_id as lookup for PATCH/GET and resolve 404 errors

--- users_app/api/views.py
+++ users_app/api/views.py
@@ -2,15 +2,26 @@ from rest_framework import generics, permissions
 from users_app.models import UserProfile
 from .serializers import UserProfileSerializer
 from .permissions import IsOwnerProfile
+from django.shortcuts import get_object_or_404
 
 class UserProfileDetailView(generics.RetrieveUpdateAPIView):
     """
-    Allows authenticated users to retrieve or update their own profile.
+    Allows authenticated users to retrieve or update their own profile,
+    using the user's ID in the URL.
     Only the profile owner is authorized.
     """
+
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerProfile]
+    # lookup_field = 'user_id'  # <- geht auch, aber ist bei OneToOne nicht default!
+
+    def get_object(self):
+        """
+        Returns the profile for the user ID in the URL.
+        """
+        user_id = self.kwargs["pk"]
+        return get_object_or_404(UserProfile, user_id=user_id)
 
 class BusinessProfileListView(generics.ListAPIView):
     """
